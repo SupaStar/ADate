c1a1fc651f3b0655ea73dffb7516cd70cfc16559
Obed Noe Martinez Gonzalez <68682799+SupaStar@users.noreply.github.com>
2023-07-05 15:41:16 -0600

    Initial Commit

---
+++ ADate/ADateApp.swift
@@ -0,0 +1,17 @@
+//
+//  ADateApp.swift
+//  ADate
+//
+//  Created by Usuario Alquimia on 05/07/23.
+//
+
+import SwiftUI
+
+@main
+struct ADateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
